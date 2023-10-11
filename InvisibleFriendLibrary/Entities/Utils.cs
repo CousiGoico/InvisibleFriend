9f81adb4c350b8502ee4277e62ecde36b96566d1
Javier Cousiño <javier.cousino.goicolea@hotmail.com>
2023-10-11 12:39:49 +0200

    InvisibleFriend

--- InvisibleFriendLibrary/Entities/Utils.cs
+++ InvisibleFriendLibrary/Entities/Utils.cs
@@ -1,8 +1,18 @@
+using System.Net;
+using System.Net.Mail;
+
 namespace InvisibleFriendLibrary;
 
 public class Utils{
     public static void SendEmail(string sender, string body){
-
+        var smtpClient = new SmtpClient("smtp.gmail.com")
+        {
+            Port = 587,
+            Credentials = new NetworkCredential("username", "password"),
+            EnableSsl = true,
+        };
+            
+        smtpClient.Send("email", "recipient", "subject", "body");
     }    
 
     public static void SendEmail(Friend friend){
