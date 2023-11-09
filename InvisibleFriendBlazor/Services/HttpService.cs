b6e421950fcb8be65619994a65ef82785a1e8185
Javier Cousiño <javier.cousino.goicolea@hotmail.com>
2023-11-09 20:15:56 +0100

    Bind model

--- InvisibleFriendBlazor/Services/HttpService.cs
+++ InvisibleFriendBlazor/Services/HttpService.cs
@@ -1,7 +1,15 @@
+using System.Net.Http.Json;
+
 namespace InvisibleFriendBlazor.Services{
 
-    public class HttpService{
+    public static class HttpService{
 
-        
+        public static async Task<T?> Get<T>(string url){
+            using (var client = new HttpClient())
+            {
+                return await client.GetFromJsonAsync<T>(url);
+            }
+
+        }
     }
 }
