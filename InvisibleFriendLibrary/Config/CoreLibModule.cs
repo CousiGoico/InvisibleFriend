c501f85e606cea0efb2011b1d4e65bc8ea0c1c48
Javier Cousiño <javier.cousino.goicolea@hotmail.com>
2023-11-11 13:56:56 +0100

    InvisibleFriend

--- InvisibleFriendLibrary/Config/CoreLibModule.cs
+++ InvisibleFriendLibrary/Config/CoreLibModule.cs
@@ -1,11 +1,13 @@
 using Autofac;
 using InvisibleFriendLibrary.Domain;
+using InvisibleFriendLibrary.Entities;
 using InvisibleFriendLibrary.Services;
 
 public class CoreLibModule: Autofac.Module{
     protected override void Load(ContainerBuilder builder)
     {
         base.Load(builder);
+
         builder.RegisterType<FriendDomain>().As<IFriendDomain>().SingleInstance();
         builder.RegisterType<GameDomain>().As<IGameDomain>().SingleInstance();
         builder.RegisterType<SmtpDomain>().As<ISmtpDomain>().SingleInstance();
